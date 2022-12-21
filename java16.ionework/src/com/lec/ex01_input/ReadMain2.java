71dc177c72014656c3264e5a17222a1ce0a67bce
Park joonseong <xdc1542@naver.com>
2022-12-21 21:08:12 +0900

    java.ionework

---
+++ java16.ionework/src/com/lec/ex01_input/ReadMain2.java
@@ -0,0 +1,25 @@
+package com.lec.ex01_input;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class ReadMain2 {
+
+	public static void main(String[] args) throws Exception {
+		// 2. read(byte[] b)
+		InputStream is = new FileInputStream("src/com/lec/ex01_input/news.txt");
+		int readByteNo;
+		
+		byte[] read_bytes = new byte[20];
+		
+		readByteNo = is.read(read_bytes);
+//		readByteNo = is.read(read_bytes, 2, 3);
+		
+		for(int i=0;i<read_bytes.length;i++) {
+			System.out.println((char)read_bytes[i]);
+		}
+		is.close();
+	}
+
+}
