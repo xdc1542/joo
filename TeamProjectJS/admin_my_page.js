0af8d7e8d5041513dfd3105480c0cec26bd15420
Park joonseong <xdc1542@naver.com>
2023-02-07 20:51:54 +0900

    TeamProjectJS

---
+++ TeamProjectJS/admin_my_page.js
@@ -0,0 +1,186 @@
+/**
+* Template Name: Logis - v1.3.0
+* Template URL: https://bootstrapmade.com/logis-bootstrap-logistics-website-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+  
+    /**
+     * Preloader
+     */
+    const preloader = document.querySelector('#preloader');
+    if (preloader) {
+      window.addEventListener('load', () => {
+        preloader.remove();
+      });
+    }
+  
+    
+    /**
+     * Scroll top button
+     */
+    const scrollTop = document.querySelector('.scroll-top');
+    if (scrollTop) {
+      const togglescrollTop = function() {
+        window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+      }
+      window.addEventListener('load', togglescrollTop);
+      document.addEventListener('scroll', togglescrollTop);
+      scrollTop.addEventListener('click', window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      }));
+    }
+  
+    /**
+     * Mobile nav toggle
+     */
+    const mobileNavShow = document.querySelector('.mobile-nav-show');
+    const mobileNavHide = document.querySelector('.mobile-nav-hide');
+  
+    document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+      el.addEventListener('click', function(event) {
+        event.preventDefault();
+        mobileNavToogle();
+      })
+    });
+  
+    function mobileNavToogle() {
+      document.querySelector('body').classList.toggle('mobile-nav-active');
+      mobileNavShow.classList.toggle('d-none');
+      mobileNavHide.classList.toggle('d-none');
+    }
+  
+    /**
+     * Hide mobile nav on same-page/hash links
+     */
+    document.querySelectorAll('#navbar a').forEach(navbarlink => {
+  
+      if (!navbarlink.hash) return;
+  
+      let section = document.querySelector(navbarlink.hash);
+      if (!section) return;
+  
+      navbarlink.addEventListener('click', () => {
+        if (document.querySelector('.mobile-nav-active')) {
+          mobileNavToogle();
+        }
+      });
+  
+    });
+  
+    /**
+     * Toggle mobile nav dropdowns
+     */
+    const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+  
+    navDropdowns.forEach(el => {
+      el.addEventListener('click', function(event) {
+        if (document.querySelector('.mobile-nav-active')) {
+          event.preventDefault();
+          this.classList.toggle('active');
+          this.nextElementSibling.classList.toggle('dropdown-active');
+  
+          let dropDownIndicator = this.querySelector('.dropdown-indicator');
+          dropDownIndicator.classList.toggle('bi-chevron-up');
+          dropDownIndicator.classList.toggle('bi-chevron-down');
+        }
+      })
+    });
+  
+    /**
+     * Initiate pURE cOUNTER
+     */
+    new PureCounter();
+  
+    /**
+     * Initiate glightbox
+     */
+    const glightbox = GLightbox({
+      selector: '.glightbox'
+    });
+  
+    /**
+     * Init swiper slider with 1 slide at once in desktop view
+     */
+    new Swiper('.slides-1', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    });
+  
+    /**
+     * Animation on scroll function and init
+     */
+    function aos_init() {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+      });
+    }
+    window.addEventListener('load', () => {
+      aos_init();
+    });
+  
+  });
+
+  var cellPhone = document.getElementById('sign_ph');
+  cellPhone.onkeyup = function(event){
+  event = event || window.event;
+  var _val = this.value.trim();
+  this.value = autoHypenPhone(_val);
+  }
+  
+  
+  function ValidChecked() {
+  
+    var n_RegExp = /^[가-힣a-zA-Z\s]+$/;
+    var p_RegExp = /^(?:(010-\d{4})|(01[1|6|7|8|9]-\d{3,4}))-(\d{4})$/; // 아직 완성은 안됨!
+  
+  
+    var objName = document.getElementById("sign_name");
+    var objPhoneNumber = document.getElementById("sign_ph");
+  
+  
+         //이름 유효성 검사
+       if (objName.value == "") {
+         alert("이름을 입력하세요.");
+         objName.focus();
+         return false;
+       };
+       if (!n_RegExp.test(objName.value)) {
+         alert("이름은 한글 2~15자까지 가능합니다.");
+         objName.focus();
+         return false;
+       };
+  
+    //   휴대폰번호 유효성검사
+       /*if (objPhoneNumber.value == "") {
+            alert("휴대폰번호를 입력하세요.");
+            objPhoneNumber.focus();
+            return false;
+          };
+       if(!p_RegExp.test(objPhoneNumber)){
+           alert("휴대폰번호가 올바르지 않습니다.");
+           objPhoneNumber.focus();
+           return false;
+         }
+         return true;*/
+}
